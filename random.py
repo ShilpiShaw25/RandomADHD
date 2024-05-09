69b7b6197613219d25792eaa7319450cd3857c8c
Ayushmak02 <83110177+Ayushmak02@users.noreply.github.com>
2024-05-10 01:40:19 +0530

    Update random.py

--- random.py
+++ random.py
@@ -23,6 +23,22 @@ def predict(input_features):
 
 # Streamlit UI
 def main():
+    page_bg_img = f"""
+<style>
+[data-testid="stAppViewContainer"] > .main {{
+background-image: url("https://i.postimg.cc/4xgNnkfX/Untitled-design.png");
+background-size: cover;
+background-position: center center;
+background-repeat: no-repeat;
+background-attachment: local;
+}}
+[data-testid="stHeader"] {{
+background: rgba(0,0,0,0);
+}}
+</style>
+"""
+
+    st.markdown(page_bg_img, unsafe_allow_html=True)
     st.title('ADHD Prediction App')
     st.write('Please provide the following information to predict:')
     
